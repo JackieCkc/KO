f7cae8d51a2242d5ea219e15a82d747046db891c
jackie <jackieckc38@gmail.com>
2020-01-27 20:05:16 +0800

    Some fixes

--- store_events/store_events.sql
+++ store_events/store_events.sql
@@ -1,45 +1,72 @@
 CREATE OR REPLACE FUNCTION get_events_aggr() 
 RETURNS TABLE (
   app_id int,
-  app_veiws int,
+  app_views int,
   app_installs int,
   app_conversion_rate decimal
-)
-AS $$
+) AS $$
 DECLARE
-    app_id_row record;
+    app_user_row record;
     event_type_row record;
-    views integer := 0;
-    prev_views integer := 0;
+    app_views integer := 0;
+    user_views integer := 0;
     installs integer := 0;
+    curr_app_id integer := -1;
+    prev_app_id integer := -1;
+    curr_user_id integer := -1;
+    prev_user_id integer := -1;
 BEGIN
-FOR app_id_row IN
-	-- get unique app ids
-	SELECT e1.app_id
+FOR app_user_row IN
+	-- get unique (app id, user id)
+	SELECT e1.app_id, e1.user_id
 	FROM store_events e1
-	GROUP BY e1.app_id
-	ORDER BY e1.app_id ASC
+	GROUP BY e1.app_id, e1.user_id
+	ORDER BY e1.app_id ASC, e1.user_id ASC
 LOOP
 	FOR event_type_row IN
-		-- iterate each events for an app id
+		-- iterate each events for every (app id, user_id)
 		SELECT e2.event_type
 		FROM store_events e2
-		WHERE e2.app_id = app_id_row.app_id
+		WHERE
+			e2.app_id = app_user_row.app_id AND
+			e2.user_id = app_user_row.user_id
 		ORDER BY e2.event_time_utc ASC
 	LOOP
+		curr_app_id = app_user_row.app_id;
+		curr_user_id = app_user_row.user_id;
+
+		IF curr_app_id != prev_app_id AND prev_app_id != -1 THEN
+			-- return an aggregation row for the previous app
+		   	RETURN QUERY
+				SELECT
+					prev_app_id AS app_id,
+			      	app_views AS app_views,
+			       	installs app_installs,
+			       	CAST(installs AS decimal)/NULLIF(app_views, 0) AS app_conversion_rate;
+			
+			-- reset count for different events
+			app_views = 0;
+		   	installs = 0;
+		   	user_views = 0;
+		END IF;
+
+		if curr_user_id != prev_user_id THEN
+			-- reset user views for a different user
+		   	user_views = 0;
+		END IF;
+
 		IF event_type_row.event_type = 'store_app_view' THEN
 			-- handle app views
-			-- prev_views is used to tracking 
-			views = views + 1;
-			prev_views = prev_views + 1;
+			app_views = app_views + 1;
+			user_views = user_views + 1;
 		END IF;
 		
 		IF event_type_row.event_type = 'store_app_download' THEN
 			-- handle installs for early versions
 			-- if there is an app view before, count it as an install
-			IF prev_views > 0 THEN
+			IF user_views > 0 THEN
 				installs = installs + 1;
-				prev_views = prev_views - 1;
+				user_views = user_views - 1;
 			END IF;
 		END IF;
 		
@@ -47,22 +74,19 @@ LOOP
 			-- handle installs for later versions
 			installs = installs + 1;
 		END IF;
-	END LOOP;
 
+		prev_app_id = app_user_row.app_id;
+		prev_user_id = app_user_row.user_id;
+	END LOOP;
+END LOOP;
+	-- return an aggregation row for the last app
 	RETURN QUERY
 		SELECT
-			app_id_row.app_id AS app_id,
-	      	views AS app_veiws,
-	       	installs app_installs,
-	       	CAST(installs AS decimal)/NULLIF(views, 0) AS app_conversion_rate;
+			prev_app_id AS app_id,
+	      	app_views AS app_views,
+	       	installs AS app_installs,
+	       	CAST(installs AS decimal)/NULLIF(app_views, 0) AS app_conversion_rate;
+END;
+$$ LANGUAGE 'plpgsql';
 
-   	-- reset variables
-   	views = 0;
-   	prev_views = 0;
-   	installs = 0;
-END LOOP;
-END; $$ 
- 
-LANGUAGE 'plpgsql';
-
-select * FROM get_events_aggr();
+select * from get_events_aggr();
--- store_events/store_events_test.sql
+++ store_events/store_events_test.sql
@@ -12,8 +12,8 @@ VALUES
 -- test store_open, store_app_update, store_fetch_manifest are not processed
 -- expect {app_id: 1, app_views: 0, app_installs: 0, app_conversion_rate: NULL}
 (1,1,'store_open','2020-01-24 20:08:08.323242'),
-(2,1,'store_app_updat','2020-01-24 20:11:01.215674'),
-(3,1,'store_fetch_manifest','2020-01-24 20:11:01.215674'),
+(1,1,'store_app_updat','2020-01-24 20:11:01.215674'),
+(1,1,'store_fetch_manifest','2020-01-24 20:11:01.215674'),
 
 -- app 2
 -- test store_app_view
@@ -29,27 +29,45 @@ VALUES
 -- test store_app_view + store_app_download
 -- expect {app_id: 4, app_views: 1, app_installs: 1, app_conversion_rate: 1}
 (1,4,'store_app_view','2020-01-24 20:11:01.215674'),
-(2,4,'store_app_download','2020-01-24 20:12:01.753111'),
+(1,4,'store_app_download','2020-01-24 20:12:01.753111'),
 
 -- app 5
 -- test store_app_download before store_app_view
 -- expect {app_id: 5, app_views: 1, app_installs: 0, app_conversion_rate: 0}
-(2,5,'store_app_download','2020-01-27 17:46:01.329535'),
+(1,5,'store_app_download','2020-01-27 17:46:01.329535'),
 (1,5,'store_app_view','2020-01-27 17:47:01.329535'),
 
 -- app 6
--- test store_app_view + store_app_download + app_installs
+-- test store_app_view + store_app_download + store_app_install
 -- expect {app_id: 6, app_views: 1, app_installs: 2, app_conversion_rate: 2}
-(3,6,'store_app_view','2020-01-27 17:47:19.499385'),
-(2,6,'store_app_download','2020-01-27 17:48:00.524156'),
+(1,6,'store_app_view','2020-01-27 17:47:19.499385'),
+(1,6,'store_app_download','2020-01-27 17:48:00.524156'),
 (1,6,'store_app_install','2020-01-27 17:48:00.524156'),
 
 -- app 7
--- test store_app_view * 4 + store_app_download + app_installs
+-- test store_app_view * 4 + store_app_download + store_app_install
 -- expect {app_id: 7, app_views: 4, app_installs: 2, app_conversion_rate: 0.5}
-(11,7,'store_app_view','2020-01-27 17:48:29.290573'),
-(12,7,'store_app_view','2020-01-27 17:48:29.290573'),
-(11,7,'store_app_view','2020-01-27 17:48:29.290573'),
-(12,7,'store_app_view','2020-01-27 17:48:29.290573'),
-(13,7,'store_app_download','2020-01-27 17:49:00.83342'),
-(14,7,'store_app_install','2020-01-27 17:49:00.83342');
+(1,7,'store_app_view','2020-01-27 17:48:29.290573'),
+(1,7,'store_app_view','2020-01-27 17:48:29.290573'),
+(1,7,'store_app_view','2020-01-27 17:48:29.290573'),
+(1,7,'store_app_view','2020-01-27 17:48:29.290573'),
+(1,7,'store_app_download','2020-01-27 17:49:00.83342'),
+(1,7,'store_app_install','2020-01-27 17:49:00.83342'),
+
+-- app 8
+-- test store_app_view + store_app_download for different users
+-- expect {app_id: 8, app_views: 1, app_installs: 0, app_conversion_rate: 0}
+(1,8,'store_app_view','2020-01-27 17:48:29.290573'),
+(2,8,'store_app_download','2020-01-27 17:49:29.290573'),
+
+-- app 9
+-- test all combinations for different users
+-- expect {app_id: 9, app_views: 2, app_installs: 3, app_conversion_rate: 1.5}
+(1,9,'store_app_view','2020-01-27 17:48:29.290573'),
+(2,9,'store_app_download','2020-01-27 17:49:29.290573'),
+(1,9,'store_app_download','2020-01-27 17:50:29.290573'),
+(2,9,'store_app_view','2020-01-27 17:51:29.290573'),
+(3,9,'store_app_install','2020-01-27 17:52:29.290573'),
+(2,9,'store_app_download','2020-01-27 17:53:29.290573'),
+(3,9,'store_app_download','2020-01-27 17:54:29.290573'),
+(2,9,'store_app_download','2020-01-27 17:55:29.290573');
